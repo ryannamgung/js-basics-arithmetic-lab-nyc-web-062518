04da5d9e349cfaf81bd412b2d612fe652a7e49e0
ryannamgung <ryannamgung@gmail.com>
2018-08-04 22:50:17 -0400

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ Variables defined in the test suite:
 - currentAge
 
 You should NOT try to redefine any of the above variables.
+Note that numbers can be a little wonky because they are manipulated using the IE64 Floating Point Binary Format, 1/10 isn't really 1/10 etc.
 */
 
 // Write your code below this line!
+
+let newID = parseInt("10000000" + oldID, 10);
+
+let ageIsValid = Number.isInteger(currentAge);
+
+let randomNumber = Math.random() * 20
+
+let randomInteger = Math.floor(randomNumber)
+
+let randomUserID = parseInt("10000000" + randomNumber, 10);
